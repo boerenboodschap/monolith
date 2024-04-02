823279197a31cdeacb83b62f9d63706eeca6bc65
TeunSpithoven <teun.spithoven@gmail.com>
2024-04-02 07:44:38 +0200

    remove node_modules

--- directus-node/node_modules/@directus/api/dist/telemetry/utils/get-user-count.d.ts
+++
@@ -1,7 +0,0 @@
-import { type Knex } from 'knex';
-export interface UserCount {
-    admin: number;
-    app: number;
-    api: number;
-}
-export declare const getUserCount: (db: Knex) => Promise<UserCount>;
